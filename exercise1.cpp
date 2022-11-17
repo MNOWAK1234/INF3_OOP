ab7bed8c7e172045b90d5f8e72b197c6c2bc9aac
MNOWAK1234 <108874597+MNOWAK1234@users.noreply.github.com>
2022-11-17 16:22:58 +0100

    Add files via upload

---
+++ exercise1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+#include <string.h>
+
+using namespace std;
+
+class Car
+{
+private:
+    double recommendedPerformance;
+public:
+    virtual string state()=0;
+    Car tuning(double addedPerformance)return Car(this.recommendedPerformance+addedPerformance)
+};
+class Steering: public Car{
+private:
+    double angle;
+    double velocity;
+    double thresholdPerformance;
+public:
+    Steering brake(double distanceToObject)
+    {
+        return Steering(0,velocity*distanceToObject/10.0, thresholdPerformance);
+    }
+    string state()
+    {
+        string relation;
+        if(velocity/angle>thresholdPerformance)relation=" above ";
+        else relation=" below ";
+        return to_string(abs(velocity/angle-thresholdPerformance))+relation+"threshold performance.";
+    }
+    Steering(double angle, double velocity, double thresholdPerformance)
+    {
+        this->angle=angle;
+        this->velocity=velocity;
+        this->thresholdPerformance=thresholdPerformance;
+    }
+
+};
+
+int main()
+{
+    Steering Tesla(2.67,56,10);
+    cout<<Tesla.state()<<endl;
+}
