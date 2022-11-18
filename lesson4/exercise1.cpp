1de8f65a30c3e771ce6ea81f0cc249a5a0e63a72
MNOWAK1234 <108874597+MNOWAK1234@users.noreply.github.com>
2022-11-18 20:52:10 +0100

    Update exercise1.cpp

--- lesson4/exercise1.cpp
+++ lesson4/exercise1.cpp
@@ -1,6 +1,9 @@
+///updated version: 18.11.22
+///compiler couldn't handle to_string() function, therefore it is removed and Car.state() returns now pair<double,string>
+
 #include <iostream>
 #include <cmath>
-#include <string.h>
+#include <string>
 
 using namespace std;
 
@@ -9,8 +12,8 @@ class Car
 private:
     double recommendedPerformance;
 public:
-    virtual string state()=0;
-    Car tuning(double addedPerformance)return Car(this.recommendedPerformance+addedPerformance)
+    virtual pair<double,string> state()=0;
+    //Car tuning(double addedPerformance)return Car(this.recommendedPerformance+addedPerformance)
 };
 class Steering: public Car{
 private:
@@ -22,12 +25,13 @@ public:
     {
         return Steering(0,velocity*distanceToObject/10.0, thresholdPerformance);
     }
-    string state()
+    pair<double,string> state()
     {
         string relation;
         if(velocity/angle>thresholdPerformance)relation=" above ";
         else relation=" below ";
-        return to_string(abs(velocity/angle-thresholdPerformance))+relation+"threshold performance.";
+        relation+="threshold performance.";
+        return make_pair(abs(velocity/angle-thresholdPerformance),relation);
     }
     Steering(double angle, double velocity, double thresholdPerformance)
     {
@@ -41,7 +45,8 @@ public:
 int main()
 {
     Steering Tesla(2.67,56,10);
-    cout<<Tesla.state()<<endl;
-    Steering Mercedes(3.43,66,14);
-    Car MercedesUpgrade=Mercedes.tuning(4);
+    pair<double,string> info=Tesla.state();
+    cout<<info.first<<info.second<<endl;
+    //Steering Mercedes(3.43,66,14);
+    //Car MercedesUpgrade=Mercedes.tuning(4);
 }
